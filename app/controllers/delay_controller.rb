af6d65684c32e2b578bb5887bbceb0a7d9e2de51
Rob Murray <robmurray17@gmail.com>
2015-02-28 17:24:04 +0000

    add post method that redirects with 307; *believe* this is ~~valid http~~ :o

--- app/controllers/delay_controller.rb
+++ app/controllers/delay_controller.rb
@@ -3,11 +3,25 @@ require_relative 'base_controller'
 module Slowly
   class DelayController < BaseController
     get '/delay/:delay/*' do |delay, _rest|
-      request = DelayRequest.new(delay, params[:splat].first)
-      logger.info "Handle request: #{request}"
+      request_params = DelayRequest.new(delay, params[:splat].first)
+      log_request(request_params)
 
-      sleep request.delay_secs
-      redirect request.url
+      sleep request_params.delay_secs
+      redirect request_params.url
+    end
+
+    post '/delay/:delay/*' do |delay, _rest|
+      request_params = DelayRequest.new(delay, params[:splat].first)
+      log_request(request_params)
+
+      sleep request_params.delay_secs
+      redirect request_params.url, 307
+    end
+
+    private
+
+    def log_request(request_params)
+      logger.info "Handle #{request.request_method} request: #{request_params}"
     end
   end
 end
