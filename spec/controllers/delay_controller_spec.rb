1116f521aaff470e3b30ef0646bbf5270ab467d5
Rob Murray <robmurray17@gmail.com>
2015-03-03 13:47:12 +0000

    Add POST specs similar to GET ;)

--- spec/controllers/delay_controller_spec.rb
+++ spec/controllers/delay_controller_spec.rb
@@ -68,4 +68,82 @@ describe Slowwly::DelayController do
       end
     end
   end
+
+  context 'with a POST request' do
+    context 'with delay time' do
+      context 'with url' do
+        before(:each) do
+          post('/delay/1500/http://example.com')
+        end
+
+        it 'responds with redirect' do
+          expect(last_response).to be_redirect
+        end
+
+        it 'responds with correct header value' do
+          expect(last_response.location).to eq('http://example.com')
+        end
+
+        context 'with post data' do
+          before(:each) do
+            post('/delay/1500/http://example.com', {key: 'value', other_key: 'something else'})
+          end
+
+          it 'does not moan about bad request, invalid params, blah' do
+            expect(last_response).to be_redirect
+          end
+        end
+      end
+
+      context 'without url' do
+        before(:each) do
+          post('/delay/1500/')
+        end
+
+        xit 'informs client of error with request' do
+          expect(last_response).to be_bad_request
+        end
+      end
+    end
+
+    context 'given a request with invalid delay' do
+      before(:each) do
+        post('/delay/foo/http://example.com')
+      end
+
+      it 'responds with redirect' do
+        expect(last_response).to be_redirect
+      end
+
+      it 'responds with correct header value' do
+        expect(last_response.location).to eq('http://example.com')
+      end
+    end
+
+    context 'given a request without delay time' do
+      context 'and url' do
+        before(:each) do
+          post('/delay//http://example.com')
+        end
+
+        xit 'responds with redirect' do
+          expect(last_response).to be_redirect
+        end
+
+        xit 'responds with correct header value' do
+          expect(last_response.location).to eq('http://example.com')
+        end
+      end
+
+      context 'without url' do
+        before(:each) do
+          post('/delay/')
+        end
+
+        it 'informs client of invalid path' do
+          expect(last_response).to be_not_found
+        end
+      end
+    end
+  end
 end
