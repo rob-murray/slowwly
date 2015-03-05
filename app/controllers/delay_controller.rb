742fe1a81e95b1bb014275aa78f861b99189865f
Rob Murray <robmurray17@gmail.com>
2015-03-05 18:54:00 +0000

    Tidy up DeejayController; bit of duplication but we will erradicate later :o

--- app/controllers/delay_controller.rb
+++ app/controllers/delay_controller.rb
@@ -1,18 +1,18 @@
 require_relative 'base_controller'
-
+require 'byebug'
 module Slowwly
   class DelayController < BaseController
-    get '/delay/:delay/*' do |delay, _rest|
-      set_request_params(delay)
-      log_request(request_params)
+    get '/delay/:delay/*' do |*args|
+      set_request_params
+      log_request
 
       sleep request_params.delay_secs
       redirect request_params.url
     end
 
-    post '/delay/:delay/*' do |delay, _rest|
-      set_request_params(delay)
-      log_request(request_params)
+    post '/delay/:delay/*' do |*args|
+      set_request_params
+      log_request
 
       sleep request_params.delay_secs
       redirect request_params.url, 307
@@ -22,14 +22,14 @@ module Slowwly
 
     attr_reader :request_params
 
-    def set_request_params(delay)
+    def set_request_params
       @request_params ||= DelayRequest.new(
-        delay: delay,
+        delay: params[:delay],
         url: params[:splat].first
       )
     end
 
-    def log_request(request_params)
+    def log_request
       logger.info "Handle #{request.request_method} request: #{request_params}"
     end
   end
